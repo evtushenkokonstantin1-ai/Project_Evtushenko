68a9c6da9577209dfc84d022208c003ae98b253a
evtushenkokonstantin1-ai <evtushenkokonstantin1@gmail.com>
2025-10-08 17:36:17 +0300

    до оценки удовлетворения критериям

--- pz_2/pz_2_1.py
+++ pz_2/pz_2_1.py
@@ -1,1 +1,6 @@
-print("Hello GitHab")
+try:
+    L = (float(input("Введите длину в сантиметрах: ")))
+    A = L // 100
+    print(A)
+except ValueError:
+    print("Ошибка! Использован неверный тип данных.")
